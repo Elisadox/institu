c5cbf8a4a5507f830dd5e00ffed444abc1332458
Elisadox <152091133+Elisadox@users.noreply.github.com>
2025-05-14 16:17:14 +0200

    third

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,17 @@
 var hand = document.querySelector(".hand");
 var box1 = document.querySelector(".box1");
+var box2 = document.querySelector(".box2");
+var box3 = document.querySelector(".box3");
+var box4 = document.querySelector(".box4");
+var under_title = document.querySelector(".under_title");
+var title = document.querySelector(".title");
 
 hand.addEventListener("click", function () {
   box1.classList.add("is-active");
-});
-
-var hand = document.querySelector(".hand");
-var box2 = document.querySelector(".box2");
-
-hand.addEventListener("click", function () {
   box2.classList.add("is-active");
-});
-
-var hand = document.querySelector(".hand");
-var box3 = document.querySelector(".box3");
-
-hand.addEventListener("click", function () {
-  box3.classList.add("is-active");
-});
-
-var hand = document.querySelector(".hand");
-var box4 = document.querySelector(".box4");
-
-hand.addEventListener("click", function () {
   box4.classList.add("is-active");
+  box3.classList.add("is-active");
+  under_title.classList.add("is-active");
+  title.classList.add("is-active");
+  hand.classList.add("is-active");
 });
