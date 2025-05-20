eae8e2ab50591c102fa93cd445e48a4d1beeb17b
Elisadox <152091133+Elisadox@users.noreply.github.com>
2025-05-20 08:57:42 +0200

    reload

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,25 @@ var box4 = document.querySelector(".box4");
 var under_title = document.querySelector(".under_title");
 var title = document.querySelector(".title");
 
+let isAnimated = false;
+
 hand.addEventListener("click", function () {
-  box1.classList.add("is-active");
-  box2.classList.add("is-active");
-  box4.classList.add("is-active");
-  box3.classList.add("is-active");
-  under_title.classList.add("is-active");
-  title.classList.add("is-active");
-  hand.classList.add("is-active");
+  if (!isAnimated) {
+    box1.classList.add("is-active");
+    box2.classList.add("is-active");
+    box4.classList.add("is-active");
+    box3.classList.add("is-active");
+    under_title.classList.add("is-active");
+    title.classList.add("is-active");
+    hand.classList.add("is-active");
+  } else {
+    box1.classList.remove("is-active");
+    box2.classList.remove("is-active");
+    box3.classList.remove("is-active");
+    box4.classList.remove("is-active");
+    under_title.classList.remove("is-active");
+    title.classList.remove("is-active");
+    hand.classList.remove("is-active");
+  }
+  isAnimated = !isAnimated;
 });
